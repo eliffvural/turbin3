218d4549a0d4999555aa826e999bad2b4a7d493c
Elif Vural <elifvural019@gmail.com>
2024-07-28 09:05:59 +0300

    wba

--- programs/wba_prereq.ts
+++ programs/wba_prereq.ts
@@ -1,40 +1,46 @@
-export type WbaPrereq =  = { "version": "0.1.0", "name": "wba_prereq", ...etc };
-export const IDL: WbaPrereq = { "version": "0.1.0", "name": "wba_prereq", ...etc };
-
-
-import { Connection, Keypair } from "@solana/web3.js"
-import wallet from "./dev-wallet.json"
-import { WbaPrereq } from "./programs/wba_prereq"
-
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
-
-const connection = new Connection("https://api.devnet.solana.com");
-
-const github = Buffer.from("<your github account>", "utf8");
-
-const provider = new AnchorProvider(connection, new Wallet(keypair), { commitment: "confirmed" });
-
-const program: Program<WbaPrereq> = new Program(IDL, provider);
-
-const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
-const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(enrollment_seeds, program.programId);
-
-(async () => {
-    try {
-      const txhash = await program.methods
-        .complete(github)
-
-        .accounts({
-            signer: keypair.publicKey,
-          })
-          .signers([
-            keypair
-          ]).rpc();
-
-          console.log(`Success! Check out your TX here:
-            https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
-
-        } catch (e) {
-            console.error(`Oops, something went wrong: ${e}`)
+export type WbaPrereq = {
+    version: "0.1.0";
+    name: "wba_prereq";
+    instructions: [
+      {
+        name: "complete";
+        accounts: [
+          {
+            name: "signer";
+            isMut: true;
+            isSigner: true;
           }
-        })();
+        ];
+        args: [
+          {
+            name: "github";
+            type: "bytes";
+          }
+        ];
+      }
+    ];
+  };
+  
+  export const IDL: WbaPrereq = {
+    version: "0.1.0",
+    name: "wba_prereq",
+    instructions: [
+      {
+        name: "complete",
+        accounts: [
+          {
+            name: "signer",
+            isMut: true,
+            isSigner: true
+          }
+        ],
+        args: [
+          {
+            name: "github",
+            type: "bytes"
+          }
+        ]
+      }
+    ]
+  };
+  
